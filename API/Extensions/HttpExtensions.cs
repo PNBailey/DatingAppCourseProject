0406cb21fb4d17c41c2eb87e98949489c9aa3f73
Paul Bailey <52pbailey@gmail.com>
2021-01-26 18:29:02 +0000

    Amended pagination reponse header

--- API/Extensions/HttpExtensions.cs
+++ API/Extensions/HttpExtensions.cs
@@ -13,7 +13,11 @@ namespace API.Extensions
         {
             var paginationHeader = new PaginationHeader(currentPage, itemsPerPage, totalItems, totalPages);
 
-            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader)); // This adds the header to the response. We have to serialize this because when we add this our response headers take a key and string value. So the ""Pagination" will be our key and our JsonSerializer.Serialize(paginationHeader) is our value. 
+            var options = new JsonSerializerOptions { // This changes the header response json data to camel case. We pass these options to our serialize method below
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase 
+            };
+
+            response.Headers.Add("Pagination", JsonSerializer.Serialize(paginationHeader, options)); // This adds the header to the response. We have to serialize this because when we add this our response headers take a key and string value. So the ""Pagination" will be our key and our JsonSerializer.Serialize(paginationHeader) is our value. 
 
             response.Headers.Add("Access-Control-Expose-Headers", "Pagination"); // When adding a custom header, we need toa dd a CORS header onto this to make this header available. The second argument hear is the name of the header we are 'exposing'. In this case it i our custom "Pagination" header. 
         }
